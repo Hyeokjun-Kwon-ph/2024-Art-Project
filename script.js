4da6891e0014c8a19ffde1de502ea142dd73dcec
Hyeokjun-Kwon-ph <kwonhj506@gmail.com>
2024-11-29 01:29:36 +0900

    Increased the gravitational constant from 5000 to 10000.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let apples = [];
 let currentApple = null;
 let origin;
-let gravitationalConstant = 5000;
+let gravitationalConstant = 10000;
 let atmosphereLayer;
 let radiusThreshold;
 let appleRadius
